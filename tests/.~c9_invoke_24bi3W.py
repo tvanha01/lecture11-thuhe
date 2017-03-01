0fbbe10278a0d6c3ca38ba66575371e4cfe9daed
Tommy Ha <tha@csumb.edu>
2017-03-01 03:16:37 +0000

    add unit tests

--- tests/.~c9_invoke_24bi3W.py
+++ tests/.~c9_invoke_24bi3W.py
@@ -5,14 +5,13 @@ class ChatBotResponseTest(unittest.TestCase):
     def test_not_command(self):
         response = functions.get_chatbot_response('Potato')
         self.assertEquals(response, '')
+    d
+    def test_first_command(self):
+        response = functions.get_chatbot_response('hello')
+        self.assertEquals(response, 'h' )
     
-    def test_add_command(self):
-        response = functions.get_chatbot_response('!! add 5 1')
-        self.assertEquals(response, '51' )
     
-    # def test_divide_command(self):
-    #     response = functions.get_chatbot_response('!! add divide 5 1')
-        
+    
     
 
 if __name__ == '__main__':
--- tests/.~c9_invoke_xQpFsX.py
+++ tests/.~c9_invoke_xQpFsX.py
@@ -6,13 +6,12 @@ class ChatBotResponseTest(unittest.TestCase):
         response = functions.get_chatbot_response('Potato')
         self.assertEquals(response, '')
     
-    def test_add_command(self):
-        response = functions.get_chatbot_response('!! add 5 1')
-        self.assertEquals(response, '51' )
+    def test_first_command(self):
+        response = functions.get_chatbot_response('hello')
+        self.assertEquals(response, p)
     
-    # def test_divide_command(self):
-    #     response = functions.get_chatbot_response('!! add divide 5 1')
-        
+    
+    
     
 
 if __name__ == '__main__':
